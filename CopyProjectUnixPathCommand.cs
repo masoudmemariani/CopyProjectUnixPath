c1aaea047e9b793c27dde0602ef0fb4d93756197
Masoud Memariani <masood13@gmail.com>
2023-01-09 13:36:56 +0100

    Add project files.

---
+++ CopyProjectUnixPathCommand.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using EnvDTE;
+using Microsoft.VisualStudio;
+using Task = System.Threading.Tasks.Task;
+
+namespace CopyProjectUnixPath
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CopyProjectUnixPathCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("dfd2b3e4-b5ac-415a-8b2b-160f1cce3b8a");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CopyProjectUnixPathCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CopyProjectUnixPathCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CopyProjectUnixPathCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in Command1's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new CopyProjectUnixPathCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        ///
+        /// 
+
+        private void Execute2(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var _dte = ServiceProvider.GetServiceAsync(typeof(EnvDTE.DTE)).Result as EnvDTE.DTE;
+
+            Array _projects = _dte.ActiveSolutionProjects as Array;
+            if (_projects.Length != 0 && _projects != null)
+            {
+                Project _selectedProject = _projects.GetValue(0) as Project;
+                //get the project path
+                string _directoryPath = new FileInfo(_selectedProject.FullName).DirectoryName;
+
+                Clipboard.SetText(ReplaceBackslashesWithSlashes(_directoryPath));
+            }
+            else
+            {
+                Console.WriteLine("No Project in solution or selected");
+            }
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "ItemContextCommand";
+
+            IntPtr hierarchyPointer, selectionContainerPointer;
+            Object selectedObject = null;
+            IVsMultiItemSelect multiItemSelect;
+            uint projectItemId;
+
+            IVsMonitorSelection monitorSelection =
+                (IVsMonitorSelection)Package.GetGlobalService(
+                    typeof(SVsShellMonitorSelection));
+
+            monitorSelection.GetCurrentSelection(out hierarchyPointer,
+                out projectItemId,
+                out multiItemSelect,
+                out selectionContainerPointer);
+
+            IVsHierarchy selectedHierarchy = Marshal.GetTypedObjectForIUnknown(
+                hierarchyPointer,
+                typeof(IVsHierarchy)) as IVsHierarchy;
+
+            if (selectedHierarchy != null)
+            {
+
+                ErrorHandler.ThrowOnFailure(selectedHierarchy.GetProperty(
+                    projectItemId,
+                    (int)__VSHPROPID.VSHPROPID_ExtObject,
+                    out selectedObject));
+            }
+
+            Project selectedProject = selectedObject as Project;
+
+            string _directoryPath = new FileInfo(selectedProject.FullName).DirectoryName;
+
+            Clipboard.SetText(ReplaceBackslashesWithSlashes(_directoryPath));
+        }
+
+        private string ReplaceBackslashesWithSlashes(string text)
+        {
+            return text.Replace("\\", "/");
+        }
+    }
+}
